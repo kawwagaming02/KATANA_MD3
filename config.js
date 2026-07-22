ecb76da6deb30e499577678b18add5b0559ef296
kawwagaming02 <kawwagaming45@gmail.com>
2026-07-22 22:15:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "dYFiDBzQ#2ssg2jgSnBpy-EmHLq2ErIgVumnM9j8Wb1ArWNFCjOI",
+  SESSION_ID: process.env.SESSION_ID || "oF8yXbzI#LFY5R2-lUT8J6lMj8zZSZhQvxKQOef-BkiyxxeZoQeY",
   MONGODB: process.env.MONGODB || "mongodb://mongo:habtEKdHITgbsKhXNpaHojOIRyZkjYYR@sakura.proxy.rlwy.net:46961",
   OWNER_NUM: process.env.OWNER_NUM || "94761495926",
 };
